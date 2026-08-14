cd09e52563489e5cb97533d2c2ae347f32de1575
abulais1344 <abulaiskhateeb@gmail.com>
2026-08-15 04:36:08 +0530

    chore: bump SW to v43 to invalidate stale cache from soda-promo fix session

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v42';
+const CACHE_VERSION = 'v43';
 
 self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', () => self.clients.claim())
